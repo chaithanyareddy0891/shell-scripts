995d0901ed9a92e77ab915725be65261267bef5c
chaithanyareddy <chaithanya.reddy0891@gmail.com>
2025-09-04 09:41:11 +0530

    testing arguments in 04-variables.sh

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1: Hi $PERSON2, how are you?"
+echo "$PERSON2: I’m good, $PERSON1. How about you?"
+echo "$PERSON1: Doing well. Did you finish the project work?"
+echo "$PERSON2: Almost, just the final report is pending."
+echo "$PERSON1: Great. Let me know if you need help."
+echo "$PERSON2: Sure, thanks for asking."
