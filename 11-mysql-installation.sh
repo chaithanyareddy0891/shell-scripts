78a28cff75625806b3c27ce42d19632ddcee06a1
chaithanyareddy <chaithanya.reddy0891@gmail.com>
2025-09-04 11:41:18 +0530

    11-mysql.sh

--- 11-mysql-installation.sh
+++ 11-mysql-installation.sh
@@ -2,6 +2,21 @@
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-    echo "please run this script with root priveleges"
+    echo "try with root previlizes,root previlizes are missing "
+    exit 1
+fi
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    echo "mysql is not avilable installing it"
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "mysql installation is not success..check it"
+        exit 1
+    else
+        echo "mysql installation is success"
+    fi
 else
-    echo "user have root previlizes"
+    echo "mysql is allready available, nothing to do"
+fi
