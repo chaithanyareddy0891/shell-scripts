4d36eab228f0991046a9d54daed51bcdcdaf1c71
chaithanyareddy <chaithanya.reddy0891@gmail.com>
2025-09-04 09:19:26 +0530

    adding echo command to conversation script

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-Ramesh: Hi Suresh, how are you?
-Suresh: I’m good, Ramesh. How about you?
-Ramesh: Doing well. Did you finish the project work?
-Suresh: Almost, just the final report is pending.
-Ramesh: Great. Let me know if you need help.
-Suresh: Sure, thanks for asking.
+echo Ramesh: Hi Suresh, how are you?
+echo Suresh: I’m good, Ramesh. How about you?
+echo Ramesh: Doing well. Did you finish the project work?
+echo Suresh: Almost, just the final report is pending.
+echo Ramesh: Great. Let me know if you need help.
+echo Suresh: Sure, thanks for asking.
