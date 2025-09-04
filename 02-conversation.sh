a6193c68be1e624b91b3426cfe0a7901d86a19fe
chaithanyareddy <chaithanya.reddy0891@gmail.com>
2025-09-04 09:13:45 +0530

    adding conversation script

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+Ramesh: Hi Suresh, how are you?
+Suresh: I’m good, Ramesh. How about you?
+Ramesh: Doing well. Did you finish the project work?
+Suresh: Almost, just the final report is pending.
+Ramesh: Great. Let me know if you need help.
+Suresh: Sure, thanks for asking.
